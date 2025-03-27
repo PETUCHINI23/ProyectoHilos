27e4ef858b94ae777621b1f1bf445f5689b71a34
Cuanary <l.raoc050223@itses.edu.mx>
2025-03-27 09:31:40 -0600

    este es el push del proyecto

---
+++ src/main/java/mx/edu/itsescjmra/hilox/Hilox.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.edu.itsescjmra.hilox;
+
+/**
+ *
+ * @author petuc
+ */
+public class Hilox {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
